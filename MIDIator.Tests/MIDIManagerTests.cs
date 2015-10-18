23414404e14e9c5e9aa42038cb3f118ef50adcca
NOVA <shootdaj@gmail.com>
2015-10-18 18:32:15 -0400

    Under way...

---
+++ MIDIator.Tests/MIDIManagerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MIDIator.Tests
+{
+	public class MIDIManagerTests
+	{
+		[Fact]
+		public void Devices_ReturnsSomeDevices()
+		{
+			var devices = MIDIManager.AvailableDevices;
+		}
+
+		[Fact]
+		public void AddChannelRecievedAction_Works()
+		{
+			MIDIManager.AddChannelRecievedAction(3, (sender, args) =>
+			{
+				Assert.True(true);
+			});
+
+			MIDIManager.StartRecording(3);
+			Thread.Sleep(15000);
+		}
+    }
+}
--- MIDIator/MIDIManager.cs
+++ MIDIator/MIDIManager.cs
@@ -8,9 +8,9 @@ using Sanford.Multimedia.Midi;
 
 namespace MIDIator
 {
-	public class MIDIManager
+	public static class MIDIManager
 	{
-		public static IEnumerable<dynamic> Devices
+		public static IEnumerable<dynamic> AvailableDevices
 		{
 			get
 			{
@@ -22,9 +22,56 @@ namespace MIDIator
 					returnValue.MID = InputDevice.GetDeviceCapabilities(i).mid;
 					returnValue.PID = InputDevice.GetDeviceCapabilities(i).pid;
 					returnValue.Support = InputDevice.GetDeviceCapabilities(i).support;
+					returnValue.DeviceID = i;
 					yield return returnValue;
 				}
 			}
+		}
+		
+
+		public static IList<InputDevice> DevicesInUse { get; } = new List<InputDevice>();
+
+		public static void AddChannelRecievedAction(int deviceID, EventHandler<ChannelMessageEventArgs> action)
+		{
+			var device = GetNewOrExistingDevice(deviceID);
+			device.ChannelMessageReceived += action;
+		}
+
+		public static void StartRecording(int deviceID)
+		{
+			if (DeviceExists(deviceID))
+			{
+				DevicesInUse.Single(device => device.DeviceID == deviceID).StartRecording();
+			}
+		}
+
+		public static void StopRecording(int deviceID)
+		{
+			if (DeviceExists(deviceID))
+			{
+				DevicesInUse.Single(device => device.DeviceID == deviceID).StopRecording();
+			}
+		}
+
+		private static InputDevice GetNewOrExistingDevice(int deviceID)
+		{
+			//new device
+			if (!DeviceExists(deviceID))
+			{
+				var device = new InputDevice(deviceID);
+                DevicesInUse.Add(device);
+				return device;
+			}
+			//existing device
+			else
+			{
+				return DevicesInUse.Single(device => device.DeviceID == deviceID);
+			}
+		}
+
+		private static bool DeviceExists(int deviceID)
+		{
+			return DevicesInUse.Select(x => x.DeviceID).Contains(deviceID);
 		}
 	}
 }
