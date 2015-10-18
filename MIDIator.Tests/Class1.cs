a88853c77b2feeb081d335e8c18525d570e5eed2
NOVA <shootdaj@gmail.com>
2015-10-18 17:30:14 -0400

    Initial Commit

---
+++ MIDIator.Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MIDIator.Tests
+{
+	public class MIDIManagerTests
+	{
+		[Fact]
+		public void Devices_ReturnsSomeDevices()
+		{
+			var devices = MIDIManager.Devices;
+		}
+	}
+}
---
+++ MIDIator/MIDIManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sanford.Multimedia.Midi;
+
+namespace MIDIator
+{
+	public class MIDIManager
+	{
+		public static IEnumerable<dynamic> Devices
+		{
+			get
+			{
+				for (int i = 0; i < InputDevice.DeviceCount; i++)
+				{
+					dynamic returnValue = new ExpandoObject();
+					returnValue.Name = InputDevice.GetDeviceCapabilities(i).name;
+					returnValue.DriverVersion = InputDevice.GetDeviceCapabilities(i).driverVersion;
+					returnValue.MID = InputDevice.GetDeviceCapabilities(i).mid;
+					returnValue.PID = InputDevice.GetDeviceCapabilities(i).pid;
+					returnValue.Support = InputDevice.GetDeviceCapabilities(i).support;
+					yield return returnValue;
+				}
+			}
+		}
+	}
+}
