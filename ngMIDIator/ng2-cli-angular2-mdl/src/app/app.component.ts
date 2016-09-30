ff620041ec51d9adf54d73cd1b27400875a601ef
Anshul Vishwakarma <shootdaj@gmail.com>
2016-09-30 01:16:24 -0700

    Much progress. Able to get devices from API. Created base UI structure. Need to add translations. Added loader screen.

--- ngMIDIator/ng2-cli-angular2-mdl/src/app/app.component.ts
+++ ngMIDIator/ng2-cli-angular2-mdl/src/app/app.component.ts
@@ -6,13 +6,42 @@ import {
     Validators,
     FormBuilder
 } from '@angular/forms';
+import {Injectable} from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import './rxjs-operators';
+
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+    constructor(private http: Http) {
+        this.getAvailableInputDevices();
+		this.getAvailableOutputDevices();
+    }
+
     public inputDevice = new FormControl('');
-    availableInputDevices = [{ Name: 'Numark Orbit' }, { Name: 'TouchOSC' }];
+
+	public availableInputDevices: Observable<Array<any>>;
+	public availableOutputDevices: Observable<Array<any>>;
+	
+    public getAvailableInputDevices() {
+	    this.http.get('http://localhost:9000/midi/AvailableInputDevices')
+		    .map(response => response.json())
+		    .subscribe(data => this.availableInputDevices = data,
+			    err => console.log(err));
+    }
+
+	public getAvailableOutputDevices() {
+		this.http.get('http://localhost:9000/midi/AvailableOutputDevices')
+			.map(response => response.json())
+			.subscribe(data => this.availableOutputDevices = data,
+				err => console.log(err));
+	}
+    
 }
 
--- ngMIDIator/ng2-cli-angular2-mdl/src/app/app.module.ts
+++ ngMIDIator/ng2-cli-angular2-mdl/src/app/app.module.ts
@@ -3,20 +3,21 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MdlModule } from 'angular2-mdl';
-
+//import { Observable } from 'rxjs/Observable';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    MdlModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        MdlModule
+        //,Observable
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
