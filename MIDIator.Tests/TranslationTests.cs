7c5db0f896b0a3d6fe792cf34bc6762145945014
Anshul Vishwakarma <shootdaj@gmail.com>
2017-01-08 21:12:01 -0800

    Fixed tests.

--- MIDIator.Tests/TranslationTests.cs
+++ MIDIator.Tests/TranslationTests.cs
@@ -12,7 +12,7 @@ namespace MIDIator.Tests
         public void Translation_Serialize()
         {
 	        var expectedSerialization =
-				"{\r\n  \"inputMessageMatchTarget\": {\r\n    \"$type\": \"ChannelMessage\",\r\n    \"command\": \"NoteOn\",\r\n    \"midiChannel\": 1,\r\n    \"data1\": 1,\r\n    \"data2\": 0\r\n  },\r\n  \"outputMessageTemplate\": {\r\n    \"$type\": \"ChannelMessage\",\r\n    \"command\": \"ProgramChange\",\r\n    \"midiChannel\": 1,\r\n    \"data1\": 2,\r\n    \"data2\": 0\r\n  },\r\n  \"translationFunction\": \"DirectTranslation\",\r\n  \"inputMatchFunction\": \"CatchAll\",\r\n  \"enabled\": true\r\n}";
+                "{\r\n  \"inputMessageMatchTarget\": {\r\n    \"$type\": \"ChannelMessage\",\r\n    \"command\": \"NoteOn\",\r\n    \"midiChannel\": 1,\r\n    \"data1\": 1,\r\n    \"data2\": 0\r\n  },\r\n  \"outputMessageTemplate\": {\r\n    \"$type\": \"ChannelMessage\",\r\n    \"command\": \"ProgramChange\",\r\n    \"midiChannel\": 1,\r\n    \"data1\": 2,\r\n    \"data2\": 0\r\n  },\r\n  \"translationFunction\": \"DirectTranslation\",\r\n  \"inputMatchFunction\": \"CatchAll\",\r\n  \"enabled\": true,\r\n  \"collapsed\": false\r\n}";
 
             var translation = new Translation(new ChannelMessage(ChannelCommand.NoteOn, 1, 1),
                 new ChannelMessage(ChannelCommand.ProgramChange, 1, 2),
@@ -27,7 +27,7 @@ namespace MIDIator.Tests
 		public void Translation_Deserialize()
 		{
 			var	serializedTranslation =
-				"{\r\n  \"inputMessageMatchTarget\": {\r\n    \"$type\": \"ChannelMessage\",\r\n    \"command\": \"NoteOn\",\r\n    \"midiChannel\": 1,\r\n    \"data1\": 1,\r\n    \"data2\": 0\r\n  },\r\n  \"outputMessageTemplate\": {\r\n    \"$type\": \"ChannelMessage\",\r\n    \"command\": \"ProgramChange\",\r\n    \"midiChannel\": 1,\r\n    \"data1\": 2,\r\n    \"data2\": 0\r\n  },\r\n  \"translationFunction\": \"DirectTranslation\",\r\n  \"inputMatchFunction\": \"CatchAll\"\r\n}";
+                "{\r\n  \"inputMessageMatchTarget\": {\r\n    \"$type\": \"ChannelMessage\",\r\n    \"command\": \"NoteOn\",\r\n    \"midiChannel\": 1,\r\n    \"data1\": 1,\r\n    \"data2\": 0\r\n  },\r\n  \"outputMessageTemplate\": {\r\n    \"$type\": \"ChannelMessage\",\r\n    \"command\": \"ProgramChange\",\r\n    \"midiChannel\": 1,\r\n    \"data1\": 2,\r\n    \"data2\": 0\r\n  },\r\n  \"translationFunction\": \"DirectTranslation\",\r\n  \"inputMatchFunction\": \"CatchAll\",\r\n  \"enabled\": true,\r\n  \"collapsed\": false\r\n}";
 
 			//left here for reference - this is the object used to create serializedTranslation by serializing it
 			//var translation = new Translation(new ChannelMessage(ChannelCommand.NoteOn, 1, 1),
